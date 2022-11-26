2ed3cf9465ef80b929a214a986bb6f751886fd2f
dipana0324 <np03cs4a220186@heraldcollege.edu.np>
2022-11-26 08:34:32 -0800

    by_dipana

--- AreaOfFourWalls.java
+++ AreaOfFourWalls.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class AreaOfFourWalls {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        float l = 4;
-        float b = 6;
+        System.out.println("Enter l: ");
+        float l = sc.nextFloat();
+
+        System.out.println("Enter b: ");
+        float b = sc.nextFloat();
 
         double area = l * b;
 
