83a109b7b3e72a2c82c624b1581dc1106ea48191
dipana0324 <np03cs4a220186@heraldcollege.edu.np>
2022-11-26 08:31:36 -0800

    by_dipana

--- AreaOfFourWalls.java
+++ AreaOfFourWalls.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class AreaOfFourWalls {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         float l = 4;
         float b = 6;
 
