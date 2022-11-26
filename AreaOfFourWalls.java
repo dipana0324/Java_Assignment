8203ba2584b77df6e11faf0b86d1b6faed8e4d10
dipana0324 <np03cs4a220186@heraldcollege.edu.np>
2022-11-26 08:30:14 -0800

    by_dipana

---
+++ AreaOfFourWalls.java
@@ -0,0 +1,10 @@
+public class AreaOfFourWalls {
+    public static void main(String[] args){
+        float l = 4;
+        float b = 6;
+
+        double area = l * b;
+
+        System.out.println("Area of Four walls = " + area);
+    }
+}
